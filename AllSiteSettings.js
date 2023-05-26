d942d9f19c0eb3528d23949003110e33b763f26a
releasestudio <57976856+releasestudio@users.noreply.github.com>
2023-05-26 15:32:48 +1000

    Initial commit

---
+++ AllSiteSettings.js
@@ -0,0 +1,21 @@
+/* Hillsong Site */
+
+/* Foundation READ MORE BUTTON */
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+/* Popup */
+import "https://code.jquery.com/ui/1.12.1/jquery-ui.js";
